965b39b6ffecc6846348de365eb68040d298c3ee
Avishkar001 <avirandhale001@gmail.com>
2022-03-14 22:37:06 +0530

    added sockets for chat section

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const express = require("express");
 const cors = require("cors");
+const socketio = require("socket.io");
 const router = express.Router();
 const app = express();
 const fs = require("fs");
@@ -139,6 +140,82 @@ router.get("/delete", (req, res) => {
 });
 
 app.use(router);
+
+
+io.on("connect", (socket) => {
+  socket.on("join", ({ name, sessionid }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, sessionid });
+
+    if (error) return callback(error);
+
+    socket.join(user.sessionid);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to sessionid ${user.sessionid}.`,
+    });
+    socket.broadcast.to(user.sessionid).emit("message", {
+      user: "admin",
+      text: `${user.name} has joined!`,
+    });
+
+    io.to(user.sessionid).emit("sessionidData", {
+      sessionid: user.sessionid,
+      users: getUsersInsessionid(user.sessionid),
+    });
+
+    callback();
+  });
+  socket.on("join-video", ({ id, sessionidId }) => {
+    console.log(id, sessionidId);
+    socket.join(sessionidId);
+    socket.to(sessionidId).broadcast.emit("user-connected", id);
+    socket.on("disconnect", () => {
+      socket.to(sessionidId).broadcast.emit("user-disconnected", id);
+    });
+  });
+  socket.on("send-ace-changes", (newValue) => {
+    console.log("Changes : ", newValue);
+    socket.broadcast.emit("receive-ace-changes", newValue);
+  });
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.sessionid).emit("message", { user: user.name, text: message });
+
+    callback();
+  });
+  socket.on("get-document", (documentId) => {
+    const data = "";
+    // Implement getDocument function
+    socket.join(documentId);
+
+    socket.emit("load-document", data);
+
+    socket.on("send-text-changes", (delta) => {
+      socket.broadcast.to(documentId).emit("receive-text-changes", delta);
+    });
+    //Implement save changes event
+  });
+
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.sessionid).emit("message", {
+        user: "Admin",
+        text: `${user.name} has left.`,
+      });
+      io.to(user.sessionid).emit("sessionidData", {
+        sessionid: user.sessionid,
+        users: getUsersInsessionid(user.sessionid),
+      });
+    }
+  });
+});
+
+
+
 server.listen(process.env.PORT || 5000, () =>
   console.log(`Server has started.`)
 );
